6dc4eaaaf28c8b2a051b038a8c5fc49f4d29a919
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2023-02-04 13:04:34 +0100

    25 done

--- src/koans/25_threading_macros.clj
+++ src/koans/25_threading_macros.clj
@@ -14,53 +14,53 @@
   (map :a coll))
 
 (meditations
- "We can use thread first for more readable sequential operations"
- (= __
+  "We can use thread first for more readable sequential operations"
+  (= {:a 1}
     (-> {}
-        (assoc :a 1)))
+      (assoc :a 1)))
 
- "Consider also the case of strings"
- (= __
+  "Consider also the case of strings"
+  (= "Hello world, and moon, and stars"
     (-> "Hello world"
-        (str ", and moon")
-        (str ", and stars")))
+      (str ", and moon")
+      (str ", and stars")))
 
- "When a function has no arguments to partially apply, just reference it"
- (= __
+  "When a function has no arguments to partially apply, just reference it"
+  (= "String with a trailing space"
     (-> "String with a trailing space "
-        clojure.string/trim))
+      clojure.string/trim))
 
- "Most operations that take a scalar value as an argument can be threaded-first"
- (= __
+  "Most operations that take a scalar value as an argument can be threaded-first"
+  (= 6
     (-> {}
-        (assoc :a 1)
-        (assoc :b 2)
-        (assoc :c {:d 4
-                   :e 5})
-        (update-in [:c :e] inc)
-        (get-in [:c :e])))
+      (assoc :a 1)
+      (assoc :b 2)
+      (assoc :c {:d 4
+                 :e 5})
+      (update-in [:c :e] inc)
+      (get-in [:c :e])))
 
- "We can use functions we have written ourselves that follow this pattern"
- (= __
+  "We can use functions we have written ourselves that follow this pattern"
+  (= 1
     (-> {}
-        (assoc :a 1)
-        (function-that-takes-a-map "hello" "there")))
+      (assoc :a 1)
+      (function-that-takes-a-map "hello" "there")))
 
- "We can also thread last using ->>"
- (= __
+  "We can also thread last using ->>"
+  (= [2 3 4]
     (->> [1 2 3]
-         (map inc)))
+      (map inc)))
 
- "Most operations that take a collection can be threaded-last"
- (= __
+  "Most operations that take a collection can be threaded-last"
+  (= 12
     (->> a-list
-         (map inc)
-         (filter even?)
-         (into [])
-         (reduce +)))
+      (map inc)
+      (filter even?)
+      (into [])
+      (reduce +)))
 
- "We can use functions we have written ourselves that follow this pattern"
- (= __
+  "We can use functions we have written ourselves that follow this pattern"
+  (= [1 2 3]
     (->> a-list-with-maps
-         (function-that-takes-a-coll "hello" "there")
-         (into []))))
+      (function-that-takes-a-coll "hello" "there")
+      (into []))))
